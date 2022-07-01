f5708e26191dab9df70484be6e9b3af6f447ec76
Harry Edgar <harryedgar@hotmail.co.uk>
2022-07-01 19:43:43 +0100

    Refactor and add win conditions

--- main.rb
+++ main.rb
@@ -1,48 +1,60 @@
-player_turn = 1
+turn = 1
+marker = ["error", "X", "O"]
+selected = []
+$win_checker = "123456789"
+board = " ---+---+---\n| 1 | 2 | 3 |
+ ---+---+---\n| 4 | 5 | 6 |\n ---+---+---
+| 7 | 8 | 9 |\n ---+---+---"  
 
-def update_board(selection=nil, x_or_o=nil)
-
-tiles = {tl: "1", tm: "2", tr: "3", 
-         ml: "4", mm: "5", mr:"6", 
-         bl: "7", bm: "8", br: "9"}  
-
-  if selection == nil && x_or_o == nil
-    draw_board(tiles)
+def win_check(win_checker, selected)
+win_conditions = [
+(/X{3}....../), (/...X{3}.../), (/......X{3}/), (/X..X..X../), (/.X..X..X./), (/..X..X..X/), 
+(/X...X...X/), (/..X.X.X../),(/O{3}....../), (/...O{3}.../), (/......O{3}/), (/O..O..O../), (/.O..O..O./), (/..O..O..O/),
+(/O...O...O/), (/..O.O.O../)]
+  
+  for x in win_conditions
+    if $win_checker.match(x)
+      puts "Game Over!"
+      abort("To play again, please restart the script")
+    elsif selected.length == 8
+      puts "Draw!"
+      abort("To play again, please restart the script")
+    end
+  end
+end
+    
+def update_board(selection, marker, board, selected)
+  if selection == nil || marker == nil
+    puts "Error, no input"
   else
-    tiles[tiles.find { |key, value| value.include?(selection) }.first] = "X"
-    draw_board(tiles)
+    board.gsub!(selection, marker)
+    $win_checker.gsub!(selection, marker)
+    puts board
+    win_check($win_checker, selected)
   end
 end
 
-
-def draw_board(tiles)
-
-puts board = 
-" ---+---+---
-| #{tiles[:tl]} | #{tiles[:tm]} | #{tiles[:tr]} |
- ---+---+---
-| #{tiles[:ml]} | #{tiles[:mm]} | #{tiles[:mr]} |
- ---+---+---
-| #{tiles[:bl]} | #{tiles[:bm]} | #{tiles[:br]} |
- ---+---+---"
-
-  
-end
-
-def get_user_input(player_turn)
-  
-  available = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
-  
-  puts "Player #{player_turn} please enter your selection"
-  selection = gets.chomp
-  if selection.to_i.between?(1, 9) && available.include?(selection)
-    puts "Your selection is #{selection}"
-    available.delete(selection)
+def get_player_selection(selected, marker, turn, board)
+puts "Player #{turn}, please enter your selection"
+selection = gets.chomp
+  if selected.include?(selection) == false
+    update_board(selection, marker[turn], board, selected)
+    update_vars(selection, selected, turn, marker, board)
   else
-    puts "Invalid input"
-    get_user_input(player_turn)
+    puts "Error"
+    get_player_selection(selected, marker, turn, board)
   end
 end
 
-update_board()
-get_user_input(player_turn)
+def update_vars(selection, selected, turn, marker, board)
+selected.push(selection)
+  if turn == 1
+    turn +=1
+    get_player_selection(selected, marker, turn, board)
+  else 
+    turn -= 1
+    get_player_selection(selected, marker, turn, board)
+  end
+end
+
+get_player_selection(selected, marker, turn, board)
